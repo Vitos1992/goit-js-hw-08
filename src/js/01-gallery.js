dc7fcc70cc1f40a38d75084b50ad5dcf8990708f
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-11-01 21:23:42 +0200

    task08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -30,32 +30,3 @@ new SimpleLightbox('.gallery a', {
   showCounter: false,
   close: false,
 });
-
-// //
-
-// // Add imports above this line
-// import SimpleLightbox from 'simplelightbox';
-// // Додатковий імпорт стилів
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { galleryItems } from './gallery-items';
-// // Change code below this line
-// console.log(galleryItems);
-// const galleryRef = document.querySelector('.gallery');
-// const markup = galleryItems
-//   .map(({ preview, original, description }) => {
-//     return `
-//       <li class="gallery__item">
-//    <a class="gallery__link" href="${original}">
-//       <img class="gallery__image" src="${preview}" alt="${description}" />
-//    </a>
-// </li>
-//     `;
-//   })
-//   .join('');
-// galleryRef.insertAdjacentHTML('beforeend', markup);
-// const gallery = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   close: false,
-//   enableKeyboard: true,
-// });

f45e23ca3664713dee9916d35c0a153386151fbf
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-11-02 00:26:34 +0200

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,30 +2,33 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-const gallery = document.querySelector('.gallery');
-
-const imgArr = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img 
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+    <img
       class="gallery__image"
-      src="${preview}" 
-      alt="${description}" />
-    </a>
-</li>`;
-  })
-  .join('');
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join('');
+}
 
-gallery.insertAdjacentHTML('beforeend', imgArr);
-
-new SimpleLightbox('.gallery a', {
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
+  captionPosition: 'bottom',
   captionDelay: 250,
-  showCounter: false,
-  close: false,
 });

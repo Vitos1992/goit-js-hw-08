91ce17bf1203c5322730e82f21d0e0d938c6cd5c
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2023-11-01 21:36:12 +0200

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,66 @@
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+
+// // Change code below this line
+
+// const galleryContainer = document.querySelector('.gallery');
+// const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+// galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+// // rendered items
+// function createGalleryItemsMarkup(items) {
+//   return items
+//     .map(({ preview, original, description }) => {
+//       return `
+//   <a class="gallery__item" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>
+// `;
+//     })
+//     .join('');
+// }
+
+// // use library SimpleLightbox
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionPosition: 'bottom',
+//   captionDelay: 250,
+// });
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
 // Change code below this line
 
-const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+console.log(galleryItems);
 
-// rendered items
-function createGalleryItemsMarkup(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `
-  <a class="gallery__item" href="${original}">
-    <img
+const gallery = document.querySelector('.gallery');
+
+const imgArr = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img 
       class="gallery__image"
-      src="${preview}"
-      alt="${description}"
-    />
-  </a>
-`;
-    })
-    .join('');
-}
+      src="${preview}" 
+      alt="${description}" />
+    </a>
+</li>`;
+  })
+  .join('');
 
-// use library SimpleLightbox
-const lightbox = new SimpleLightbox('.gallery a', {
+gallery.insertAdjacentHTML('beforeend', imgArr);
+
+new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionPosition: 'bottom',
   captionDelay: 250,
+  showCounter: false,
+  close: false,
 });
